f2ceee3d71c9ac38078db6573db38e5739cd5138
qulbyazz <qulbyazzumy@gmail.com>
2024-10-31 11:13:13 +0700

    update percobaan 2

--- ArrayNilai23.java
+++ ArrayNilai23.java
@@ -5,12 +5,16 @@ public class ArrayNilai23 {
         Scanner input = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan Nilai akhir ke-" +i+ ": ");
             nilaiAkhir[i] = input.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Masukkan Nilai akhir ke-" +i+ " adalah " +nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" +i+ " lulus!");
+            }else{
+                System.out.println("Mahasiswa ke-" +i+ " tidak lulus!");
+            }
         }
     }
 }
