986fc66fe6abe8c18069122b58df0707b1958b94
qulbyazz <qulbyazzumy@gmail.com>
2024-10-31 10:03:03 +0700

    update percobaan 1

--- ArrayBilangan23.java
+++ ArrayBilangan23.java
@@ -1,19 +1,20 @@
+
 import java.util.Scanner;
 
-public class ArrayBilangan23{
+public class ArrayBilangan23 {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int[] bil = new int[4];
+        // int[] bil = new int[4];
+        // bil[0] = (int) 5.0;
+        // bil[1] = 12867;
+        // bil[2] = (int) 7.5;
+        // bil[3] = 2000000;
+        int[] bil = {5, 13, -7, 17};
 
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
-
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
